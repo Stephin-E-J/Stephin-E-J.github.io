64e6404889e9bdb7e8768ab99ff73971b9113fbb
Captaino <stephinjohnson06@gmail.com>
2025-05-15 14:20:25 +0400

    website

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+/**Using js we will be able to move the image using buttons */
+/**let fuction is used to declare a variable */
 let currentIndex = 0;
-
+/**the function tag is used to calulate a certain value give to it */
   function moveImage(step) {
+    /**const is used to declare a variable but that cannot be changed */
     const slider = document.getElementById('sliderImage');
     const totalImages = slider.children.length; /*The child function is given as a child to the parent element*/
     currentIndex = (currentIndex + step + totalImages) % totalImages;
-    slider.style.transform = 'translateX(' + (-900 * currentIndex) + 'px)';
+    slider.style.transform = 'translateX(' + (-900 * currentIndex) + 'px)';/**this is used to change the size of the image container */
   }
